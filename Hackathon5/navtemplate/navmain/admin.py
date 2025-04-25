b4e15d880f113d1a688c9b679a78ca5aeea911da
Gangaajayakumar <gangaajayakumar1998@gmail.com>
2025-04-25 18:37:58 +0530

    upload navtemplate project

---
+++ Hackathon5/navtemplate/navmain/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import Incident
+
+# Register your models here.
+@admin.register(Incident)
+
+class IncidentAdmin(admin.ModelAdmin): # <- Customizing the admin display itself
+
+    list_display = ('timestamp', 'name', 'location',  'incident_type')
+
+    list_filter = ('timestamp',)
+
+    ordering = ('timestamp',)
